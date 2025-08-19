bfaaf841218f0056caee77a575d4dae27d59f797
cbrlpdr <cbrl.pdr@gmail.com>
2025-08-19 12:17:29 -0400

    feat: added filter dog feature

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,14 @@ import { cats } from "./repositories/catRepository";
 import { dogs } from "./repositories/dogRepository";
 import { request } from "http";
 
+interface Params{
+    id: string
+}
+
 const server = fastify({
     logger: true
 });
+
 
 server.get("/cats", async(request, response) => {
     response.type("application/json").code(200);
@@ -19,11 +24,8 @@ server.get("/dogs", async(request, response) => {
     return [dogs]
 })
 
-interface CatParams{
-    id: string
-}
 
-server.get<{Params: CatParams}>("/cats/:id", async(request, response) => {
+server.get<{Params: Params}>("/cats/:id", async(request, response) => {
     const id = parseInt(request.params.id);
     const cat = cats.find((c) => c.id === id);
 
@@ -35,6 +37,20 @@ server.get<{Params: CatParams}>("/cats/:id", async(request, response) => {
         return {cat}
     }
 });
+
+server.get<{Params: Params}>("/dogs/:id", async(request, response) => {
+    const id = parseInt(request.params.id);
+    const dog = dogs.find((d) => d.id === id);
+
+    if(!dog){
+        response.type("application/json").code(404);
+        return{message: 'Specified dog not found'};
+    }else{
+        response.type("application/json").code(200);
+        return {dog};
+    }
+})
+
 
 server.listen({port: 3333},
 () => {
